b0697f8e8728f972c5f4f7d128ac6f75bcfd588c
aidarous <aidarous12@gmail.com>
2022-08-08 22:21:46 -0400

    created third archetype

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let guard = {
-        "position" : "guard",
-        "move1" : "shoot",
-        "move2" : "pass",
-        "move3" : "steal"
+    "position" : "guard",
+    "move1" : "shoot",
+    "move2" : "pass",
+    "move3" : "steal"
     }
 
 let forward = {
-        "position" : "forward",
-        "move1" : "catch and shoot",
-        "move2" : "cut to basket",
-        "move3" : "help defense"
+    "position" : "forward",
+    "move1" : "catch and shoot",
+    "move2" : "cut to basket",
+    "move3" : "help defense"
     }
 
+let center = {
+    "position" : "center",
+    "move1" : "dunk",
+    "move2" : "pick and roll",
+    "move3" : "block"
+} 
