6e5cdaa1548be2c549f1a26500a54823bf0d433f
aidarous <aidarous12@gmail.com>
2022-08-08 22:03:58 -0400

    created second archetype

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-let archetypes = [
-    {
+let guard = {
         "position" : "guard",
         "move1" : "shoot",
         "move2" : "pass",
         "move3" : "steal"
-
     }
-]
+
+let forward = {
+        "position" : "forward",
+        "move1" : "catch and shoot",
+        "move2" : "cut to basket",
+        "move3" : "help defense"
+    }
+
